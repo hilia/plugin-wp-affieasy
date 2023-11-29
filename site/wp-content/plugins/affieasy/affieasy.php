942cbbd8a09613343910226a4c8c90b58e43a642
hilia <ludovic@PC-Ludo-Fixe>
2023-11-29 21:39:12 +0100

    increase meta header of this plugin

--- site/wp-content/plugins/affieasy/affieasy.php
+++ site/wp-content/plugins/affieasy/affieasy.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name: AffiEasy
  * Description: Plugin to easily and quickly generate responsive tables and manage affiliate links.
- * Version: 1.0.5
+ * Version: 1.1.4
  * Text Domain: affieasy
  * Author: Affieasy Team
  * Author URI: https://www.affieasy.com/
